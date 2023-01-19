ec4a5662e9be147ee5a8047678dccdcb1219a087
ErJump <fedelijunior@gmail.com>
2023-01-19 11:34:13 +0100

    feat: :sparkles: Adds SidebarComponent.jsx

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
+import SidebarComponent from './components/SidebarComponent.jsx';
+import './style/main.scss';
 
 function App() {
   return (
-    <>
-      <div class="row">
-        <div class="col-2">
-          <h1>React Fotoalbum</h1>
+    <div className='ms_app'>
+      <div className="row">
+        <div className="col-2">
+          <SidebarComponent/>
         </div>
-        <div class="col-10">
+        <div className="col-10">
           <h1>React Fotoalbum</h1>
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
---
+++ src/components/SidebarComponent.jsx
@@ -0,0 +1,25 @@
+import logo from "../images/jg.png"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHouse, faToolbox } from '@fortawesome/free-solid-svg-icons'
+
+function SidebarComponent() {
+    return (
+        <nav className="navbar navbar-expand-lg px-3 d-flex flex-column align-items-start ms_bg_darker h-100 position-fixed">
+            <a className="navbar-brand text-primary my-4" href="http://localhost:3000">
+                <img className="ms_logo" src={logo} alt="logo"></img>
+            </a>
+            <div className="navbar-nav d-flex flex-column">
+                <a className="ms_navlink d-block rounded-5 px-3 py-1 mb-3" href="http://localhost:3000">
+                    <FontAwesomeIcon icon={faHouse} className="display-inline-block me-3"/>
+                    Home
+                </a>
+                <a className="ms_navlink d-block rounded-5 px-3 py-1 mb-3" href="http://localhost:8080/photo">
+                    <FontAwesomeIcon icon={faToolbox} className="display-inline-block me-3"/>
+                    Backoffice
+                </a>
+            </div>
+        </nav>
+    );
+}
+
+export default SidebarComponent;
