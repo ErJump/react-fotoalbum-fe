1d1fd88765476284d75b5eddfdbf85a3d734d1d8
ErJump <fedelijunior@gmail.com>
2023-01-19 12:04:55 +0100

    feat: :sparkles: Adds HomeComponent.jsx

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import SidebarComponent from './components/SidebarComponent.jsx';
+import HomeComponent from './components/HomeComponent.jsx'
 import './style/main.scss';
 
 function App() {
@@ -6,9 +7,7 @@ function App() {
     <div className='ms_app container-fluid'>
       <div className="row">
         <SidebarComponent/>
-        <div className="col-10">
-          <h1>React Fotoalbum</h1>
-        </div>
+        <HomeComponent/>
       </div>
     </div>
   );
---
+++ src/components/HomeComponent.jsx
@@ -0,0 +1,9 @@
+function HomeComponent() {
+    return (
+        <div className="col-10 pt-4">
+            <h1>Home</h1>
+        </div>
+    );
+}
+
+export default HomeComponent;
